7dd237ab1716e02d236307f4c0229ca565666eb3
Chris Paterson <chrispaterson22@gmail.com>
2021-02-16 11:02:49 +0000

    TM RN crash course shopping list app complete

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
-import Header from './components/Header';
-import ListItem from './components/ListItem';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import 'react-native-get-random-values';
 import {v4 as uuid} from 'uuid';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
 const App = () => {
   const [items, setItems] = useState([
@@ -13,12 +14,33 @@ const App = () => {
     {id: uuid(), text: 'Juice'},
   ]);
 
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id !== id);
+    });
+  };
+
+  const addItem = (text) => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', [], {
+        cancelable: true,
+      });
+    } else {
+      setItems((prevItems) => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
-        renderItem={({item}) => <ListItem item={item} />}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
